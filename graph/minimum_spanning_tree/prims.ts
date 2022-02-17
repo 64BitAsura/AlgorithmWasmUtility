073c685cb51a2226b3804ffce462a3e9479cd928
Glitch (lofty-time-puffin) <none>
2022-02-17 20:53:07 +0000

    ⛵️💼 Checkpoint ./graph/minimum_spanning_tree/prims.ts:5133501/2722

--- graph/minimum_spanning_tree/prims.ts
+++ graph/minimum_spanning_tree/prims.ts
@@ -1,34 +1,43 @@
-
-function findMinEdgeVertex(minimumWeightedEdgeByVertex: StaticArray<i32>, mst: StaticArray<boolean>): i32{
-   let minWeight = I32.MAX_VALUE, minVertexIndex: i32;
-   mst.forEach((visited, vertexIndex)=>{
-     if(!visited && minimumWeightedEdgeByVertex[vertexIndex] < minWeight){
-       minWeight = minimumWeightedEdgeByVertex[vertexIndex];
-       minVertexIndex = vertexIndex;
-     }
-   });
-   return minVertexIndex;
+function findMinEdgeVertex(
+  minimumWeightedEdgeByVertex: StaticArray<i32>,
+  mst: StaticArray<boolean>
+): i32 {
+  let minWeight = I32.MAX_VALUE;
+  let minVertexIndex: i32 = -1;
+  mst.forEach((visited, vertexIndex) => {
+    if (!visited && minimumWeightedEdgeByVertex[vertexIndex] < minWeight) {
+      minWeight = minimumWeightedEdgeByVertex[vertexIndex];
+      minVertexIndex = vertexIndex;
+    }
+  });
+  return minVertexIndex;
 }
 
-export function mst(graph: i32[][]): i32[]{
+export function mst(graph: i32[][]): i32[] {
   const parent = new StaticArray<i32>(graph.length);
-  const minimumWeightedEdgeByVertex = new StaticArray<i32>(graph.length).fill(I8.MAX_VALUE);
+  const minimumWeightedEdgeByVertex = new StaticArray<i32>(graph.length).fill(
+    I32.MAX_VALUE
+  );
   const mst = new StaticArray<boolean>(graph.length).fill(false);
-  
-  minimumWeightedEdgeByVertex[0]=0;
-  parent[0]=-1; // starting vertex can't have parent per say
-  
-  for(let i = 0; i < graph.length-1 ; i++){
+
+  minimumWeightedEdgeByVertex[0] = 0;
+  parent[0] = -1; // starting vertex can't have parent per say
+
+  for (let i = 0; i < graph.length - 1; i++) {
     const minVertexIndex = findMinEdgeVertex(minimumWeightedEdgeByVertex, mst);
     mst[minVertexIndex] = true;
-    for(let v = 0; v < graph.length; v++){
-      if(graph[minVertexIndex][v] && mst[v] === false && graph[minVertexIndex][v] < minimumWeightedEdgeByVertex[v]){
+    for (let v = 0; v < graph.length; v++) {
+      if (
+        graph[minVertexIndex][v] &&
+        mst[v] === false &&
+        graph[minVertexIndex][v] < minimumWeightedEdgeByVertex[v]
+      ) {
         parent[v] = minVertexIndex;
         minimumWeightedEdgeByVertex[v] = graph[minVertexIndex][v];
       }
     }
   }
-  
+
   return parent.slice(1);
 }
 export const EDGE_ID = idof<i32[]>();
