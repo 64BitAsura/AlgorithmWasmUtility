7be0ebcb7e3f4bb548248d26978b5f04bfc1b27c
Glitch (lofty-time-puffin) <none>
2022-02-21 18:29:15 +0000

    🎿🍏 Checkpoint ./graph/minimum_spanning_tree/prims.test.js:5133501/316

--- graph/minimum_spanning_tree/prims.test.js
+++ graph/minimum_spanning_tree/prims.test.js
@@ -1,7 +1,11 @@
 const loader = require("@assemblyscript/loader/umd");
 const fetch = require("isomorphic-fetch");
+const fs = require("fs");
+const path = require("path");
+const wasmPath = path.resolve(__dirname, './mst.wasm');
+const buffer = fs.readFileSync(wasmPath);
 test('',()=>{
-  loader.instantiate(fetch("../mst.wasm")).then(({exports})=>{
+  loader.instantiate(buffer).then(({exports})=>{
     console.log(exports);
   }).catch((error)=> console.log(error));
 });
