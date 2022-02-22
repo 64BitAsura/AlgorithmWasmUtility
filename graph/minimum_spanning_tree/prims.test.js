4f42c9ad94a788c41de7eaa70e6f759019248283
Glitch (lofty-time-puffin) <none>
2022-02-22 14:31:35 +0000

    🏓💷 Checkpoint ./graph/minimum_spanning_tree/prims.test.js:5133501/237 ./graph/minimum_spanning_tree/prims.ts:5133501/4

--- graph/minimum_spanning_tree/prims.test.js
+++ graph/minimum_spanning_tree/prims.test.js
@@ -9,6 +9,10 @@ test('',()=>{
     const {__pin,__unpin,__newArray,__getArray, VERTEX_ID, EDGE_ID, MST_ID, MST, mst} = exports;
     const re = (__getArray(mst(__newArray(VERTEX_ID,[__newArray(EDGE_ID,[1000,1,1])
       ,__newArray(EDGE_ID,[1,1000,4]),__newArray(EDGE_ID,[1,4,1000])]))));
-    console.log(re.map((mstBuff)=> MST.wrap(__pin(mstBuff)).parent));
+    assertEqual(re.map((mstBuff)=> MST.wrap(__pin(mstBuff))).map(({parent, vertex})=>({parent, vertex})),[
+      { parent: -1, vertex: 0 },
+      { parent: 0, vertex: 1 },
+      { parent: 0, vertex: 2 }
+    ]);
   }).catch((error)=> console.log(error));
 });
