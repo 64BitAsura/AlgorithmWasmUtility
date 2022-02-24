dbe61b56d206f7289b5ecbba57a34c31e948cb66
Glitch (lofty-time-puffin) <none>
2022-02-24 16:14:00 +0000

    📒💘 Checkpoint ./algorithms/graph/minimum_spanning_tree/kruskals/index.ts:5133501/264

--- algorithms/graph/minimum_spanning_tree/kruskals/index.ts
+++ algorithms/graph/minimum_spanning_tree/kruskals/index.ts
@@ -1,3 +1,16 @@
-export function mst(graph: i32[][]): i32[]{
+class EDGE {
+  src: i32;
+  dest: i32;
+}
+
+export function mst(graph: StaticArray<StaticArray<i32>>): i32[]{
+  
+  const edges = i32[];
+  for(let u=0; u < graph.length; u++){
+    for(let v=0; v< graph.length; v++){
+      
+    }
+  }
+  
   
 }
