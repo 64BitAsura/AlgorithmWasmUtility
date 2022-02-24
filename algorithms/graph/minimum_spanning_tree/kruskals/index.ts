f6ff09ac63ddbab05ef3317bf91a8bb19bc79caf
Glitch (lofty-time-puffin) <none>
2022-02-24 17:01:58 +0000

    🌊🎑 Checkpoint ./algorithms/graph/minimum_spanning_tree/kruskals/index.ts:5133501/241

--- algorithms/graph/minimum_spanning_tree/kruskals/index.ts
+++ algorithms/graph/minimum_spanning_tree/kruskals/index.ts
@@ -1,6 +1,7 @@
 class EDGE {
   src: i32;
   dest: i32;
+  weight: i32;
 }
 
 export function mst(graph: StaticArray<StaticArray<i32>>): i32[]{
@@ -8,10 +9,16 @@ export function mst(graph: StaticArray<StaticArray<i32>>): i32[]{
   const edges = new Array<EDGE>();
   for(let u=0; u < graph.length; u++){
     for(let v=0; v< graph.length; v++){
-      edges.push({src:u, dest:v, weight: graph[u][v]});
-      graph[v][u] = Int32.MAX_VALUE;
+      if(grap[u][v]< Int32.MAX_VALUE){
+       edges.push({src:u, dest:v, weight: graph[u][v]});
+       graph[v][u] = Int32.MAX_VALUE;
+      }
     }
   }
   
+  // step 1 sort edges in non-decreasing order
+  edges.sort((x:EDGE,y:EDGE)=> x.weight - y.weight);
+  
+  const mstSet = new Set<EDGE>();
   
 }
