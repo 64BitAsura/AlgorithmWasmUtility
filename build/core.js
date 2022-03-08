b65e407c4515b8a56c5e62335f087b7057343e21
Glitch (lofty-time-puffin) <none>
2022-03-08 14:22:45 +0000

    🐇🐛 Checkpoint ./build/core.js:5133501/755 ./package.json:5133501/48

---
+++ build/core.js
@@ -0,0 +1,26 @@
+const path = require('path'), fs=require('fs');
+const exec = require('child_process').exec;
+
+function fromDir(startPath,filter){
+
+    //console.log('Starting from dir '+startPath+'/');
+
+    if (!fs.existsSync(startPath)){
+        console.log("no dir ",startPath);
+        return;
+    }
+
+    var files=fs.readdirSync(startPath);
+    for(var i=0;i<files.length;i++){
+        var filename=path.join(startPath,files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()){
+            fromDir(filename,filter); //recurse
+        }
+        else if (filename.indexOf(filter)>=0) {
+            console.log('-- found: ',filename);
+        };
+    };
+};
+
+fromDir('./algorithms','.ts');
