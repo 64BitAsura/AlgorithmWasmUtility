c862bef4c35f63bf9503c65affaceb57ef938d6d
Glitch (lofty-time-puffin) <none>
2022-03-24 19:07:17 +0000

    📡💁 Checkpoint ./algorithms/graph/minimum_spanning_tree/kruskals/index.test.js:5133501/110

--- algorithms/graph/minimum_spanning_tree/kruskals/index.test.js
+++ algorithms/graph/minimum_spanning_tree/kruskals/index.test.js
@@ -12,10 +12,11 @@ test('MST sanity',(done)=>{
   }).then(({exports})=>{
     wasm = {...exports};
     const {__pin,__unpin,__newArray,__getArray, __getString, VERTEX_ID, EDGE_ID, MST_ID, MST, mst} = exports;
-    const graph = __newArray(VERTEX_ID,[__newArray(EDGE_ID,[1000,30,10,20])
-                                        ,__newArray(EDGE_ID,[30,1000,4,1])
-                                        ,__newArray(EDGE_ID,[10,4,1000,2])
-                                        ,__newArray(EDGE_ID,[2,1,20,1000])]);
+    const graph = __newArray(VERTEX_ID,[__newArray(EDGE_ID,[1000,30,10,20,40])
+                                        ,__newArray(EDGE_ID,[30,1000,4,1,1])
+                                        ,__newArray(EDGE_ID,[10,4,1000,2,9])
+                                        ,__newArray(EDGE_ID,[2,1,20,1000,20])
+                                        ,__newArray(EDGE_ID,[40,1,9,20,1000])]);
     const mstArrayBuff = (__getArray(mst(graph)));
     
     
