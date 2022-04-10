1f6ade57ed51d20b0abffefd7a899f87f6620b8d
root <samkumar15@gmail.com>
2022-04-10 23:58:06 +0000

    wasm bindgen tinkering progress

--- algorithms/graph/minimum_spanning_tree/kruskals/lib.rs
+++ algorithms/graph/minimum_spanning_tree/kruskals/lib.rs
@@ -1,17 +1,24 @@
 use wasm_bindgen::prelude::*;
+use std::marker::{Sized};
+use std::hash::{Hash, Hasher};
+use std::rc::Rc;
+use std::cmp::Ordering;
 use crate::data_structures::graph::{Graph, Edge, VertexTrait};
 
+impl <JsValue: Sized + Eq + Hash + Ord + PartialOrd + Clone> VertexTrait for JsValue{
+}
+
 #[wasm_bindgen]
-pub fn mst<'s,K: VertexTrait>(edges: &'s Vec<&'s Edge<'s,K>>, vertices_count: i32) -> Vec<Edge<'s,K>> {
+pub fn mst(edges: Rc<Vec<Rc<Edge<JsValue>>>>, vertices_count: i32) -> Vec<Edge<JsValue>> {
     //step1 sort edges by weight in non decreasing order
-    let mut local_edges = edges.clone();
+    let mut local_edges = edges.get().clone();
     local_edges.sort_by(|a, b| b.2.cmp(&a.2));
 
     let mut edge_counts: i32 = 0;
     let mut mst_graph = Graph::new();
     //step3 repeat previous step until n-1 edges are added to mst graph, where n is vertices count
      while edge_counts < (vertices_count-1){
-        let edge = local_edges.remove(0);
+        let edge = local_edges.remove(0).get();
         edge_counts = edge_counts+1;
         // step2 check mst graph make cycles with current edge
         if mst_graph.is_cyclic(edge.0, edge.1) == false {
--- data_structures/graph.rs
+++ data_structures/graph.rs
@@ -1,42 +1,47 @@
 use std::marker::{Sized};
-use std::hash::{Hash};
+use std::hash::{Hash, Hasher};
+use std::rc::Rc;
 use std::cell::{RefCell};
+use std::cmp::Ordering;
 use std::collections::HashSet;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
+use wasm_bindgen::prelude::*;
 
 pub trait VertexTrait: Sized + Eq + Hash + Ord + PartialOrd + Clone  {}
+
 
 #[derive(PartialEq, Eq, Hash, Clone)]
 pub struct Vertex<K: VertexTrait>(K);
 
-pub struct Edge<'s, K: VertexTrait>(pub &'s Vertex<K>, pub &'s Vertex<K>, pub i32);
 
-pub struct Graph<'s,K :  VertexTrait>{
-    tree: RefCell<HashMap<&'s Vertex<K>, HashSet<&'s Vertex<K>>>>,
-    parents: RefCell<HashMap<&'s Vertex<K>, &'s Vertex<K>>>
+pub struct Edge< K: VertexTrait>(pub Rc<Vertex<K>>, pub Rc<Vertex<K>>, pub i32);
+
+pub struct Graph<K :  VertexTrait>{
+    tree: RefCell<HashMap<Rc<Vertex<K>>, HashSet<Rc<Vertex<K>>>>>,
+    parents: RefCell<HashMap<Rc<Vertex<K>>, Rc<Vertex<K>>>>
 }
 
-impl<'s,K :  VertexTrait> Graph<'s,K>{
-   pub fn new() -> Graph<'s,K>{
+impl<K :  VertexTrait> Graph<K>{
+   pub fn new() -> Graph<K>{
         Graph{
             tree: RefCell::new(HashMap::new()),
             parents: RefCell::new(HashMap::new())
         }
     }
-   pub fn add(&mut self, edge: &Edge<'s,K>){
-         let Edge(_src,_dest,_) = edge;
+   pub fn add(&mut self, edge: Rc<Edge<K>>){
+         let Edge(_src,_dest,_) = &*Rc::into_raw(edge);
          let mut adjacents = self.tree.borrow_mut().get(_src).cloned().unwrap_or(HashSet::new());
-         adjacents.insert(_dest);
-         self.tree.borrow_mut().insert(_src, adjacents);
+         adjacents.insert(*_dest);
+         self.tree.borrow_mut().insert(*_src, adjacents);
     }
-   pub fn to_edges(&self)-> Vec<Edge<'s,K>>{
+   pub fn to_edges(&self)-> Vec<Edge<K>>{
         return self.tree.borrow().iter().fold(vec![],|mut acc, (key,value)| {
-          value.iter().for_each(|vertex| acc.push(Edge(&key, &vertex, 0)));
+          value.iter().for_each(|vertex| acc.push(Edge(*key, *vertex, 0)));
           return acc;
         });
     }
-   pub fn is_cyclic(&self, _src: &'s Vertex<K>, _dest: &'s Vertex<K>)-> bool{
+   pub fn is_cyclic(&self, _src: Rc<Vertex<K>>, _dest: Rc<Vertex<K>>)-> bool{
         let x = self.find(_src);
         let y = self.find(_dest);
         let mut cyclic = false;
@@ -46,16 +51,16 @@ impl<'s,K :  VertexTrait> Graph<'s,K>{
         self.union(_src,_dest);
         return cyclic;
     }
-    fn find(&self, x: &'s Vertex<K>)-> &Vertex<K>{
+    fn find(&self, x: Rc<Vertex<K>>)-> Rc<Vertex<K>>{
         let parents =  &mut self.parents.borrow_mut();
         let entry = parents.entry(x);
         if let Entry::Vacant(_o) = entry{
            return x;
         } else {
-           return &self.find(entry.key());
+           return self.find(*entry.key());
         }
     }
-    fn union(&self, _src: &'s Vertex<K>, _dest: &'s Vertex<K>){
+    fn union(&self, _src:Rc<Vertex<K>>, _dest: Rc<Vertex<K>>){
         let _ = &self.parents.borrow_mut().insert(_src,_dest);
     }
 }
