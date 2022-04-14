49621430b2d41ed4cadc78578b9979fbc0aa6f22
root <samkumar15@gmail.com>
2022-04-14 20:29:54 +0000

    rust wasm working

--- algorithms/graph/minimum_spanning_tree/kruskals/lib.rs
+++ algorithms/graph/minimum_spanning_tree/kruskals/lib.rs
@@ -1,28 +1,44 @@
+use serde::{Serialize, Deserialize};
+use wasm_bindgen::{JsObject, JsCast};
+use wasm_bindgen::describe::WasmDescribe;
 use wasm_bindgen::prelude::*;
 use std::marker::{Sized};
 use std::hash::{Hash, Hasher};
 use std::rc::Rc;
 use std::cmp::Ordering;
-use crate::data_structures::graph::{Graph, Edge, VertexTrait};
+use crate::data_structures::graph::{Graph, Edge, Vertex, VertexTrait};
 
-impl <JsValue: Sized + Eq + Hash + Ord + PartialOrd + Clone> VertexTrait for JsValue{
+impl VertexTrait for u8{
+ 
 }
 
-#[wasm_bindgen]
-pub fn mst(edges: Rc<Vec<Rc<Edge<JsValue>>>>, vertices_count: i32) -> Vec<Edge<JsValue>> {
-    //step1 sort edges by weight in non decreasing order
-    let mut local_edges = edges.get().clone();
-    local_edges.sort_by(|a, b| b.2.cmp(&a.2));
+#[derive(Serialize, Deserialize)]
+pub struct Wasm_Edge(u8, u8, usize);
 
-    let mut edge_counts: i32 = 0;
+
+#[wasm_bindgen]
+pub fn mst_for_JS(edges_serialized: &JsValue, vertices_count: usize) -> JsValue {
+    let edges: Vec<Wasm_Edge> = edges_serialized.into_serde().unwrap();
+    let mut local_edges: Vec<Edge<u8>> = edges.iter().map(|edge|{ 
+      return Edge::new(edge.0, edge.1, edge.2);
+    }).collect();
+    let mst: Vec<Wasm_Edge> = mst(&mut local_edges, vertices_count).iter().map(|edge| Wasm_Edge(edge.0.0, edge.1.0,edge.2)).collect();
+    return JsValue::from_serde(&mst).unwrap();
+}
+
+pub fn mst<K: VertexTrait>(edges: &mut Vec<Edge<K>>, vertices_count: usize) -> Vec<Edge<K>> {
+    //step1 sort edges by weight in non decreasing order
+    edges.sort_by(|a, b| b.2.cmp(&a.2));
+
+    let mut edge_counts: usize = 0;
     let mut mst_graph = Graph::new();
     //step3 repeat previous step until n-1 edges are added to mst graph, where n is vertices count
      while edge_counts < (vertices_count-1){
-        let edge = local_edges.remove(0).get();
+        let edge = &edges[edge_counts];
         edge_counts = edge_counts+1;
         // step2 check mst graph make cycles with current edge
-        if mst_graph.is_cyclic(edge.0, edge.1) == false {
-          mst_graph.add(edge);
+        if mst_graph.is_cyclic(&edge.0, &edge.1) == false {
+          mst_graph.add(&edge);
         } 
      }
      return mst_graph.to_edges();
--- data_structures/graph.rs
+++ data_structures/graph.rs
@@ -8,40 +8,45 @@ use std::collections::HashMap;
 use std::collections::hash_map::Entry;
 use wasm_bindgen::prelude::*;
 
-pub trait VertexTrait: Sized + Eq + Hash + Ord + PartialOrd + Clone  {}
-
-
-#[derive(PartialEq, Eq, Hash, Clone)]
-pub struct Vertex<K: VertexTrait>(K);
-
-
-pub struct Edge< K: VertexTrait>(pub Rc<Vertex<K>>, pub Rc<Vertex<K>>, pub i32);
-
-pub struct Graph<K :  VertexTrait>{
-    tree: RefCell<HashMap<Rc<Vertex<K>>, HashSet<Rc<Vertex<K>>>>>,
-    parents: RefCell<HashMap<Rc<Vertex<K>>, Rc<Vertex<K>>>>
+pub trait VertexTrait: Sized + Clone + Eq + Ord +PartialOrd +Hash + Copy  {
 }
 
-impl<K :  VertexTrait> Graph<K>{
-   pub fn new() -> Graph<K>{
+#[derive(PartialEq, Clone, Eq, Ord, PartialOrd, Hash, Copy)]
+pub struct Vertex<K: VertexTrait>(pub K);
+
+pub struct Edge<K: VertexTrait>(pub Vertex<K>, pub Vertex<K>, pub usize);
+
+impl <K: VertexTrait> Edge<K>{
+  pub fn new(source: K, dest: K, weight: usize) -> Edge<K>{
+      return Edge(Vertex(source), Vertex(dest), weight);
+  }
+}
+
+pub struct Graph<'l, K :  VertexTrait>{
+    tree: RefCell<HashMap<&'l Vertex<K>, HashSet<&'l Vertex<K>>>>,
+    parents: RefCell<HashMap<&'l Vertex<K>, &'l Vertex<K>>>
+}
+
+impl<'l, K :  VertexTrait> Graph<'l, K>{
+   pub fn new() -> Graph<'l, K>{
         Graph{
             tree: RefCell::new(HashMap::new()),
             parents: RefCell::new(HashMap::new())
         }
-    }
-   pub fn add(&mut self, edge: Rc<Edge<K>>){
-         let Edge(_src,_dest,_) = &*Rc::into_raw(edge);
+   }
+   pub fn add(&mut self, edge: &'l Edge<K>){
+         let Edge(_src,_dest,_) = edge;
          let mut adjacents = self.tree.borrow_mut().get(_src).cloned().unwrap_or(HashSet::new());
-         adjacents.insert(*_dest);
-         self.tree.borrow_mut().insert(*_src, adjacents);
-    }
+         adjacents.insert(_dest);
+         self.tree.borrow_mut().insert(_src, adjacents);
+   }
    pub fn to_edges(&self)-> Vec<Edge<K>>{
         return self.tree.borrow().iter().fold(vec![],|mut acc, (key,value)| {
-          value.iter().for_each(|vertex| acc.push(Edge(*key, *vertex, 0)));
+          value.iter().for_each(|vertex| acc.push(Edge(**key, **vertex, 0)));
           return acc;
         });
-    }
-   pub fn is_cyclic(&self, _src: Rc<Vertex<K>>, _dest: Rc<Vertex<K>>)-> bool{
+   }
+   pub fn is_cyclic(&self, _src: &'l Vertex<K>, _dest: &'l Vertex<K>)-> bool{
         let x = self.find(_src);
         let y = self.find(_dest);
         let mut cyclic = false;
@@ -51,16 +56,16 @@ impl<K :  VertexTrait> Graph<K>{
         self.union(_src,_dest);
         return cyclic;
     }
-    fn find(&self, x: Rc<Vertex<K>>)-> Rc<Vertex<K>>{
+    fn find(&self, x: &'l Vertex<K>)-> &'l Vertex<K>{
         let parents =  &mut self.parents.borrow_mut();
         let entry = parents.entry(x);
         if let Entry::Vacant(_o) = entry{
            return x;
         } else {
-           return self.find(*entry.key());
+           return self.find(entry.key());
         }
     }
-    fn union(&self, _src:Rc<Vertex<K>>, _dest: Rc<Vertex<K>>){
+    fn union(&self, _src: &'l Vertex<K>, _dest: &'l Vertex<K>){
         let _ = &self.parents.borrow_mut().insert(_src,_dest);
     }
 }
