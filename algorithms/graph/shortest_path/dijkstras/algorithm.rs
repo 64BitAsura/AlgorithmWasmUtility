2aba58d47ec5e526bcf71486d4f67c95c3365f74
Sambath Kumar Logakrishnan <samkumar15@gmail.com>
2022-07-16 13:08:10 +0000

    dijkstras alogrithm wasm module

--- algorithms/graph/shortest_path/dijkstras/algorithm.rs
+++ algorithms/graph/shortest_path/dijkstras/algorithm.rs
@@ -14,8 +14,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[derive(Clone, Hash, Copy, Serialize, Deserialize, Debug)]
 struct Node<K: VertexTrait>{
     vertex: K,
-    cost: usize,
-    f: usize
+    cost: usize
 }
 
 impl <K:VertexTrait + Serialize + fmt::Display> VertexTrait for Node<K>{
@@ -23,13 +22,13 @@ impl <K:VertexTrait + Serialize + fmt::Display> VertexTrait for Node<K>{
 
 impl <K:VertexTrait + Serialize + fmt::Display> fmt::Display for Node<K>{
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    return write!(f, "Node ( vertex: {}, cost: {}, f: {} )",self.vertex, self.cost, self.f);
+    return write!(f, "Node ( vertex: {}, cost: {} )",self.vertex, self.cost);
   }
 }
 
 impl<K: VertexTrait> PartialEq for Node<K> {
   fn eq(&self, other: &Self) -> bool {
-      self.f.eq(&other.f) && self.cost.eq(&other.cost)
+      self.cost.eq(&other.cost)
   }
 }
 
@@ -38,33 +37,30 @@ impl<K: VertexTrait> Eq for Node<K> {}
 impl <K:VertexTrait>Ord for Node<K> {
        fn cmp(&self, other: &Node<K>) -> Ordering {
            // Notice that the we flip the ordering here
-           match other.f.cmp(&self.f){
-            Ordering::Equal => other.cost.cmp(&self.cost),
-            s=>s
-           }
+           other.cost.cmp(&self.cost)
        }
-   }
+}
   
-   // `PartialOrd` needs to be implemented as well.
-   impl <K:VertexTrait> PartialOrd for Node<K> {
+// `PartialOrd` needs to be implemented as well.
+impl <K:VertexTrait> PartialOrd for Node<K> {
        fn partial_cmp(&self, other: &Node<K>) -> Option<Ordering> {
            Some(self.cmp(other))
        }
-   }
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct WasmEdge(pub usize, pub usize, pub usize);
 
 fn init_node<K: VertexTrait>(vertex: K)-> Node<K>{
-    return Node{vertex: vertex, cost: 1, f:0}
+    return Node{vertex: vertex, cost: 1}
 }
 
-fn init_node_with_estimated_cost<K: VertexTrait>(vertex: K, cost: usize, f: usize)-> Node<K>{
-  return Node{vertex: vertex, cost: cost, f:f}
+fn init_node_with_estimated_cost<K: VertexTrait>(vertex: K, cost: usize)-> Node<K>{
+  return Node{vertex: vertex, cost: cost}
 }
 
 #[wasm_bindgen]
-pub fn shortest_path_for_js(edges_serialized: JsValue, start: JsValue, destination: JsValue, heuristic: &js_sys::Function)-> Result<JsValue, serde_wasm_bindgen::Error>  {
+pub fn shortest_path_for_js(edges_serialized: JsValue, start: JsValue, destination: JsValue)-> Result<JsValue, serde_wasm_bindgen::Error>  {
     let edges: Vec<WasmEdge> = serde_wasm_bindgen::from_value(edges_serialized)?;
 
     let start_node = serde_wasm_bindgen::from_value(start)?;
@@ -76,16 +72,14 @@ pub fn shortest_path_for_js(edges_serialized: JsValue, start: JsValue, destinati
 
     let sp: Vec<usize> = shortest_path(&mut local_edges, 
       start_node, 
-      destination_node, 
-       heuristic);
+      destination_node);
     return serde_wasm_bindgen::to_value(&sp);
 }
 
 use std::fmt::Debug;
 fn shortest_path<'a, K: VertexTrait + Debug>
 (edges: &mut Vec<Edge<K>>, 
-   start: K, destination: K, 
-   heuristic: &js_sys::Function)->Vec<K>{
+   start: K, destination: K)->Vec<K>{
   let mut graph = Graph::new();
   for edge in edges.iter(){
     graph.add(edge);
@@ -118,15 +112,12 @@ fn shortest_path<'a, K: VertexTrait + Debug>
     // get the neighbour vertecies 
     for (neighbour, move_cost) in graph.find_neighbours(Vertex(vertex)){
       let new_cost = cost + move_cost;
-      let h:usize;
       match parents.entry(neighbour.0){
          Entry::Vacant(e)=>{
-          h = serde_wasm_bindgen::from_value(heuristic.call1(&JsValue::undefined(), &JsValue::from_str(&vertex.to_string())).unwrap()).unwrap();
           e.insert((vertex, new_cost));
          }
          Entry::Occupied(mut e)=>{
           if e.get().1 > new_cost{
-            h = serde_wasm_bindgen::from_value(heuristic.call1(&JsValue::undefined(), &JsValue::from_str(&vertex.to_string())).unwrap()).unwrap();
             e.insert((vertex, new_cost));
           }else {
            continue;
@@ -134,7 +125,7 @@ fn shortest_path<'a, K: VertexTrait + Debug>
          }
       }
       
-      open_heap.push(init_node_with_estimated_cost(neighbour.0, new_cost, h+new_cost));
+      open_heap.push(init_node_with_estimated_cost(neighbour.0, new_cost));
       
     }
   }
---
+++ algorithms/graph/shortest_path/dijkstras/test.rs
@@ -0,0 +1,32 @@
+#[cfg(test)]
+mod test {
+use wasm_bindgen_test::*;
+use crate::algorithm::WasmEdge;
+use crate::algorithm::shortest_path_for_js;
+#[wasm_bindgen_test]
+fn pass() {
+    let mut edges = Vec::new();
+    edges.push(WasmEdge(0, 2, 10));
+    edges.push(WasmEdge(0, 1, 1));
+    edges.push(WasmEdge(1, 3, 2));
+    edges.push(WasmEdge(2, 1, 1));
+    edges.push(WasmEdge(2, 3, 3));
+    edges.push(WasmEdge(2, 4, 1));
+    edges.push(WasmEdge(3, 2, 7));
+    edges.push(WasmEdge(3, 4, 2));
+    edges.push(WasmEdge(4, 2, 1));
+    edges.push(WasmEdge(5, 3, 1));
+
+    let js_path = shortest_path_for_js(serde_wasm_bindgen::to_value(&edges).ok().unwrap(), 
+    serde_wasm_bindgen::to_value(&(0 as i32)).ok().unwrap(), 
+    serde_wasm_bindgen::to_value(&( 2 as i32)).ok().unwrap());
+    let sp: Vec<i32> = js_path.unwrap().into_serde().unwrap();
+    assert_eq!(vec![0,1,3,4,2], sp);
+
+    let js_path = shortest_path_for_js(serde_wasm_bindgen::to_value(&edges).ok().unwrap(), 
+    serde_wasm_bindgen::to_value(&(0 as i32)).ok().unwrap(), 
+    serde_wasm_bindgen::to_value(&( 3 as i32)).ok().unwrap());
+    let sp: Vec<i32> = js_path.unwrap().into_serde().unwrap();
+    assert_eq!(vec![0,1,3], sp);
+}
+}
